ff627ef5f02bd9ce89aabecaf18383e69b16b8a3
a.kataev <a.kataev@selfplus.com>
2022-11-22 13:29:17 +0500

    test runtime meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,30 @@
-import Link from 'next/link'
+import Head from "next/head";
+import {useEffect, useState} from "react";
+
+function useInBrowser() {
+    const [isInBrowser, setIsInBrowser] = useState(false);
+
+    useEffect(() => {
+        setIsInBrowser(true);
+    }, [isInBrowser]);
+
+    return isInBrowser;
+}
+
 
 export default function Home() {
+    const isBrowser = useInBrowser();
+    const TITLE = isBrowser ? "Runtime Title" : "Rendered Title";
+    const DESCRIPTION = isBrowser ? "Runtime Description" : "Rendered Description";
   return (
-    <div>
-      Hello World2. <Link href="/about">About</Link>
-    </div>
+      <>
+          <Head>
+              <title>{TITLE}</title>
+              <meta name="description" content={DESCRIPTION} />
+          </Head>
+          <div>
+               Hello World.
+          </div>
+      </>
   )
 }
